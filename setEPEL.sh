d56d2bbf00a1733f298b50d200f71f7ca5f7adb2
Bhavin Tandel <bhautandel@yahoo.com>
2016-08-31 13:29:36 +0100

    added wget

--- setEPEL.sh
+++ setEPEL.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+sudo yum install wget -y
 # download in /opt folder
 if [cat /etc/issue | grep 'Cent' | grep '6' | wc -l > 0]
 then
    cd /opt/
-   wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+   sudo wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
    # unpack it
-   rpm -ivh epel-release-latest-6.noarch.rpm
+   sudo rpm -ivh epel-release-latest-6.noarch.rpm
 
 elif [cat /etc/issue | grep 'Cent' | grep '7' | wc -l > 0]
 then 
    cd /opt/
-   wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+   sudo wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
    # unpack it
-   rpm -ivh epel-release-latest-7-8.noarch.rpm
+   sudo rpm -ivh epel-release-latest-7-8.noarch.rpm
 fi
 
 # Install ansible
-yum install ansible -y
+sudo yum install ansible -y
