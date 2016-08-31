9f7001cd854a5bc37d11685f3c18deb1198a314e
Bhavin Tandel <bhautandel@yahoo.com>
2016-08-31 12:34:03 +0100

    added support for centos 6 &  7

--- setEPEL.sh
+++ setEPEL.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 # download in /opt folder
-cd /opt/ && wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+if [cat /etc/issue | grep 'Cent' | grep '6' | wc -l > 0]
+then
+   cd /opt/
+   wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
+   # unpack it
+   rpm -ivh epel-release-latest-6.noarch.rpm
 
-# unpack it
-rpm -ivh epel-release-latest-6.noarch.rpm
+elif [cat /etc/issue | grep 'Cent' | grep '7' | wc -l > 0]
+then 
+   cd /opt/
+   wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-8.noarch.rpm
+   # unpack it
+   rpm -ivh epel-release-latest-7-8.noarch.rpm
+fi
 
 # Install ansible
 yum install ansible -y
